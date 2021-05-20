e456db1ae37a12503f5df3b6d45058690ce16768
David Wandratsch <david.wandratsch@htl-wels.at>
2021-05-20 10:41:56 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,27 @@
+//***********************************************************************
+//
+//                  H T L - W E L S   /   Klasse 3BHIT
+//
+//-----------------------------------------------------------------------
+// Programm         : CSV - Files
+//
+// Ersteller        : David Wandratsch
+//
+// Erstelldatum     : 2021-05-20
+//
+// Abgabedatum      : 2021-05-25
+//
+// Kurzbeschreibung : Arbeiten mit CSV - Dateien
+//
+// GitHub Repository: https://github.com/WandratschDavid/csvFiles
+//***********************************************************************
+
+// Including libraries
+#include <stdio.h>
+
+// main program
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
